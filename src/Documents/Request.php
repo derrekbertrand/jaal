9ea55bf276d79c62ec1e3c189fa2c293492fa2f3
Derrek Bertrand <derrekbertrand@gmail.com>
2017-04-21 21:32:00 -0400

    Deleted old trait; closes #3

---
+++ src/Documents/Request.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace DialInno\Jaal\Documents;
+
+class Request {
+    
+}
---
+++ src/Documents/Response.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DialInno\Jaal\Documents;
+
+use Illuminate\Http\Response as LaravelResponse;
+
+class Response extends LaravelResponse {
+    
+}
